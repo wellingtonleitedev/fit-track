89436ca7aea51be6c5b701d22c0819c7ba7f971e
Wellington Leite <wellingtonleitee.s@gmail.com>
2025-06-26 15:22:22 -0300

    feat: add training and exercise domain entity

--- src/FitTrack.Domain/Exercises/Exercise.cs
+++ src/FitTrack.Domain/Exercises/Exercise.cs
@@ -1,11 +1,18 @@
+using FitTrack.Domain.Trainings;
+using FitTrack.Domain.TrainingExercises;
+
 namespace FitTrack.Domain.Exercises;
 
-public class Exercise {
+public class Exercise
+{
     public Guid Id { get; set; } = Guid.NewGuid();
     public string Name { get; set; } = string.Empty;
     public string Sets { get; set; } = string.Empty;
     public string? Rest { get; set; }
     public string? Description { get; set; }
+    public List<Training> Trainings { get; set; } = [];
     public DateTime CreatedAt { get; set; } = DateTime.Now;
     public DateTime UpdatedAt { get; set; } = DateTime.Now;
+
+    public ICollection<TrainingExercise> TrainingExercise { get; set; } = [];
 }
---
+++ src/FitTrack.Domain/TrainingExercises/TrainingExercise.cs
@@ -0,0 +1,18 @@
+using FitTrack.Domain.Exercises;
+using FitTrack.Domain.Trainings;
+
+namespace FitTrack.Domain.TrainingExercises;
+
+public class TrainingExercise
+{
+    public Guid Id { get; set; }
+    public Guid ExerciseId { get; set; }
+    public Exercise Exercise { get; set; } = null!;
+
+    public Guid TrainingId { get; set; }
+    public Training Training { get; set; } = null!;
+
+    public int? Order { get; set; } = 0;
+    public DateTime CreatedAt { get; set; } = DateTime.Now;
+    public DateTime UpdatedAt { get; set; } = DateTime.Now;
+}
