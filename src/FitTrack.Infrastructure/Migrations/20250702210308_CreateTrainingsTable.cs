666720ba2023141254d880000037f3354adcf5a6
Wellington Leite <wellingtonleitee.s@gmail.com>
2025-07-02 18:10:58 -0300

    feat: add a new migration to trainings table

---
+++ src/FitTrack.Infrastructure/Migrations/20250702210308_CreateTrainingsTable.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FitTrack.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class CreateTrainingsTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Trainings",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Name = table.Column<string>(type: "TEXT", nullable: false),
+                    Day = table.Column<string>(type: "TEXT", maxLength: 10, nullable: true),
+                    CreatedAt = table.Column<DateTime>(type: "TEXT", nullable: false, defaultValueSql: "CURRENT_TIMESTAMP"),
+                    UpdatedAt = table.Column<DateTime>(type: "TEXT", nullable: false, defaultValueSql: "CURRENT_TIMESTAMP")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Trainings", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ExerciseTraining",
+                columns: table => new
+                {
+                    ExercisesId = table.Column<Guid>(type: "uuid", nullable: false),
+                    TrainingsId = table.Column<Guid>(type: "uuid", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ExerciseTraining", x => new { x.ExercisesId, x.TrainingsId });
+                    table.ForeignKey(
+                        name: "FK_ExerciseTraining_Exercises_ExercisesId",
+                        column: x => x.ExercisesId,
+                        principalTable: "Exercises",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_ExerciseTraining_Trainings_TrainingsId",
+                        column: x => x.TrainingsId,
+                        principalTable: "Trainings",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ExerciseTraining_TrainingsId",
+                table: "ExerciseTraining",
+                column: "TrainingsId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ExerciseTraining");
+
+            migrationBuilder.DropTable(
+                name: "Trainings");
+        }
+    }
+}
