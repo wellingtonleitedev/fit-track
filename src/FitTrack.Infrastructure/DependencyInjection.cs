5bed94724886b1be26bac2737fd0a15347c5158a
Wellington Leite <wellingtonleitee.s@gmail.com>
2025-07-02 18:33:47 -0300

    feat: inject workout repository

--- src/FitTrack.Infrastructure/DependencyInjection.cs
+++ src/FitTrack.Infrastructure/DependencyInjection.cs
@@ -4,11 +4,13 @@ using Microsoft.Extensions.DependencyInjection;
 using FitTrack.Infrastructure.Common.Persistence;
 using FitTrack.Infrastructure.Exercises.Persistence;
 using FitTrack.Infrastructure.Trainings.Persistence;
+using FitTrack.Infrastructure.Workouts.Persistence;
 
 public static class DependencyInjection
 {
     public static IServiceCollection AddInfrastructure(this IServiceCollection services)
     {
+        services.AddScoped<IWorkoutRepository, WorkoutRepository>();
         services.AddScoped<IExerciseRepository, ExerciseRepository>();
         services.AddScoped<ITrainingRepository, TrainingRepository>();
         services.AddDbContext<FitTrackDbContext>(options => options.UseSqlite("Data Source = FitTrack.db"));
